3d01da4fe63a04b5c00c63e90e3506693a2c7677
ComputadorasySensores <79330985+ComputadorasySensores@users.noreply.github.com>
2021-08-09 20:13:05 -0300

    Create neopixel_rpipico.py
    Codigo en CircuitPython para usar el Neopixel del Capitulo 24 de Computadoras y Sensores

---
+++ neopixel_rpipico.py
@@ -0,0 +1,22 @@
+import board
+import neopixel
+import time
+
+NUM_PIXELS = 8
+
+pixels = neopixel.NeoPixel(board.GP19, NUM_PIXELS)
+pixels.brightness = 0.2
+
+while True:
+    pixels.fill(0)
+    pixels.fill((255, 0, 0))    # rojo
+    time.sleep(2)
+    pixels.fill((0, 255, 0))    # verde
+    time.sleep(2)
+    pixels.fill((0, 0, 255))    # azul
+    time.sleep(2)
+    pixels.fill(0)
+    for i in range(NUM_PIXELS):
+        pixels[i] = 127,0,127   # violeta
+        time.sleep(0.5)
+    pixels.show()
